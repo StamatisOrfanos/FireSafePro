b8b3df76c416643c30774b8d5003418512d1e165
Stamatis Orfanos <stamatisorfanos99@gmail.com>
2024-12-04 01:28:48 +0200

    Create app correclty

--- fire_safe_pro/fire_safe_pro/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .address import Address
-from .company import Company
-from .customer import Customer, Address
-from .fire_extinguisher import FireExtinguisher
-from .order import Order, OrderItem
-from .user import User
-
-
-__all__ = ['Address', 'Company', 'Customer', 'Address', 'FireExtinguisher', 'Order', 'OrderItem', 'User']
